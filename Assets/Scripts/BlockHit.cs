c3b5a5c5c5cff07f787e428e039174b7fc936431
yann tauron <e-ytauron@etu.loc>
2024-03-20 15:40:06 +0100

    Code 20-03-2024

--- Assets/Scripts/BlockHit.cs
+++ Assets/Scripts/BlockHit.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class BlockHit : MonoBehaviour {
 
@@ -10,7 +11,12 @@ public class BlockHit : MonoBehaviour {
 
     public bool isHidden = false;
 
+    public PlatformEffector2D platformEffector2d;
+
+    public GameObject collectiblePrefab;
+
     private void Awake() {
+        platformEffector2d.enabled = isHidden;
         if (isHidden) {
             sr.color = Color.clear;
         }
@@ -28,9 +34,21 @@ public class BlockHit : MonoBehaviour {
     }
     IEnumerator Hit() {
         isAnimating = true;
+        platformEffector2d.enabled = false;
+        sr.color = Color.white;
+        MaxHits--;
         Vector3 endPosition = transform.position + Vector3.up * 0.5f;
         yield return transform.MoveBackAndForth(endPosition);
+
+        if(collectiblePrefab != null) {
+            GameObject collectible = Instantiate(collectiblePrefab, transform.position, Quaternion.identity);
+            Collectible collec = collectible.GetComponent<Collectible>();
+            collec.canBeDestroyedOnContact = false;
+            Vector3 collectibleEndPosition = collectible.transform.localPosition + Vector3.up * 1.5f;
+            yield return collectible.transform.MoveBackAndForth(collectibleEndPosition);
+            collec.Picked();
+        }
+
         isAnimating = false;
-        MaxHits--;
     }
 }
